02aad1272827556b1ff6c5f1c9d48b9e349952e0
Jaid <jaid.jsx@gmail.com>
2020-10-02 19:05:23 +0200

    Formatted
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
-import path from "path"
-
 import babelPluginTester from "babel-plugin-tester"
+import path from "path"
 
 const indexModule = process.env.MAIN ? path.resolve(__dirname, "..", process.env.MAIN) : path.join(__dirname, "..", "src")
 const {default: plugin} = require(indexModule)
